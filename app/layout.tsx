9f5f42f3906c5bf3c7730e31bee577990887b75d
Benjamin Rowe <benjaminrowe0@gmail.com>
2025-11-07 22:53:34 +1100

    another push for secuirty

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,10 @@
-import type { Metadata } from "next";
+import type { Metadata as NextMetadata } from "next";
+
+interface Metadata extends NextMetadata {
+  verification?: NextMetadata["verification"] & {
+    probely?: string;
+  };
+}
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/Footer";
