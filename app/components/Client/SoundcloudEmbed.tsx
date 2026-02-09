ae7236df1432044ba56a00ef2c02853ceb51b2c5
Benjamin <Dev@mvmntentertainment.com>
2026-02-10 08:20:33 +1100

    fixing height of the soundcloud

--- app/components/Client/SoundcloudEmbed.tsx
+++ app/components/Client/SoundcloudEmbed.tsx
@@ -29,7 +29,7 @@ export default function SoundCloudEmbed() {
     <div ref={ref} className="w-full aspect-[16/9]">
       {isVisible && (
         <iframe
-          className="w-full h-full"
+          className="w-full h-75"
           allow="autoplay; encrypted-media"
           src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/soundcloud%253Aplaylists%253A2148527099"
           frameBorder="0"
