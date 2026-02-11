1f6abf2ebb821e30a88eb1d9f0d7ba90c7cd5b1b
Benjamin <Dev@mvmntentertainment.com>
2026-02-11 17:35:11 +1100

    fixed ts bug

--- app/components/Client/BlogCard.tsx
+++ app/components/Client/BlogCard.tsx
@@ -80,7 +80,7 @@ export default function BlogCard({ item }: { item: ContentItem }) {
               unoptimized
               fill
               className="object-cover group-hover:scale-110 transition-transform duration-700"
-              alt={item.title || item.name || "Image"}
+              alt={item.title || "Image"}
             />
             <div className="absolute inset-0 bg-gradient-to-t from-black/40 to-transparent" />
           </div>
