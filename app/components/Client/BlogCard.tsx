97ffcc2c54a575beb83a60a686297d2b72bf0113
Benjamin <Dev@mvmntentertainment.com>
2026-02-12 17:48:34 +1100

    fixed ts error

--- app/components/Client/BlogCard.tsx
+++ app/components/Client/BlogCard.tsx
@@ -88,7 +88,7 @@ export default function BlogCard({ item }: { item: ContentItem }) {
             sizes="(max-width: 768px) 100vw,
      (max-width: 1200px) 50vw,
      33vw"
-              alt={item.title || item.name || "Image"}
+              alt={item.title ||  "Image"}
             />
             <div className="absolute inset-0 bg-gradient-to-t from-black/40 to-transparent" />
           </div>
