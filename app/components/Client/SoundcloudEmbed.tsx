6a155d23e3ece61a921410b7c549029ccd0134e7
Benjamin <Dev@mvmntentertainment.com>
2026-02-16 16:31:47 +1100

    Have added the new soundcloud playlist for 2026

--- app/components/Client/SoundcloudEmbed.tsx
+++ app/components/Client/SoundcloudEmbed.tsx
@@ -28,7 +28,7 @@ export default function SoundCloudEmbed() {
       {isVisible && (
         <iframe
           className="w-full h-full"
-          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/soundcloud%253Aplaylists%253A2148527099"
+          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/soundcloud%253Aplaylists%253A2190251222&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
           loading="lazy"
           allow="autoplay"
           title="SoundCloud Playlist"
