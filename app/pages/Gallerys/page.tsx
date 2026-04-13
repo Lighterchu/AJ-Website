351c7e7d5f4972079fb6b8445c2fe66bf00a3f25
Lighterchu <nextphase.digital.team@gmail.com>
2026-04-13 10:43:11 +1000

    Update page.tsx
    have removed out djs sections

--- app/pages/Gallerys/page.tsx
+++ app/pages/Gallerys/page.tsx
@@ -64,10 +64,7 @@ export default async function Events() {
         Past Events
       </h1>
       <EventList events={eventsResData} showNewEvents={false}/>
-      <h1 className="text-4xl font-extrabold my-12 text-center text-gray-900 dark:text-gray-100">
-        Our DJs 
-      </h1>
-       <Djs />
+      
     </main>
   );
 }
